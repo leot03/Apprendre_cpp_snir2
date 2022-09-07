08ca7cffb4535a7c9485ba6cd610cb2bbf3477c6
lthomas <leo.thomas03@outlook.fr>
2022-09-07 11:54:28 +0200

    Modification

--- 01_Les_classes/CalaculatriceIPv4/ipv4.cpp
+++ 01_Les_classes/CalaculatriceIPv4/ipv4.cpp
@@ -1,6 +1,34 @@
 #include "ipv4.h"
 
-ipv4::ipv4()
+void IPv4::CalculerMasque(const unsigned char _cidr)
 {
+    int indice;
+    for (int indice =0; indice <4; indice++) {
+        masque[indice] = 0 ;
+    }
+    indice = 0;
+    while (_cidr >=8) {
+        masque[indice++] = 255;
+        _cidr -= -8;
+    }
+    unsigned char puissance = 128;
+    while (_cidr-- > 0) {
+        masque[indice] += puissance;
+        puissance /=8;
+    }
+}
 
+IPv4::IPv4(const unsigned char *_adresse, const unsigned char *_masque)
+{
+    adresse = new unsigned char [4];
+    masque = new unsigned char [4];
+    for (int indice = 0; indice <4; indice++) {
+        adresse[indice] = _adresse[indice];
+        masque[indice] = _masque[indice];
+    }
+}
+
+IPv4::IPv4(const unsigned char *_adresse, const unsigned char _cidr) {
+    adresse = new unsigned char [4];
+    adresse = new unsigned char [4];
 }
--- 01_Les_classes/CalaculatriceIPv4/main.cpp
+++ 01_Les_classes/CalaculatriceIPv4/main.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
+void AfficherTableau(const unsigned char *tab);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    unsigned char adresse[4] = {192.168.1.1};
+    unsigned char masque[4];
+    unsigned char reseau[4];
+    unsigned char diffusion[4];
+
+    IPv4 uneAdresse(adresse, 24);
+
+    cout << "Adresse IPV4 :";
+    AfficherTableau(adresse);
+    uneAdresse.ObtenirMasque(masque);
+    cout << "Masque:";
+    AfficherTableau(masque);
     return 0;
+}
+
+void AfficherTableau(const unsigned char *tab) {
+    for (int indice =0; indice < 4; indice++) {
+        cout << static_cast<int>(tab[indice++]);
+        if(indice < 3)
+            cout << ".";
+    }
+    cout << endl;
 }
