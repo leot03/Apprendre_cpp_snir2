d143b8569a8ab07e7ef5cb5ed24c1d7686950a50
lthomas <leo.thomas03@outlook.fr>
2022-09-09 11:23:28 +0200

    Mise à jour fichiers

--- 01_Les_classes/MenuCPP/main.cpp
+++ 01_Les_classes/MenuCPP/main.cpp
@@ -17,84 +17,70 @@ enum CHOIX_MENU
 int main()
 {
     int choix;
-    Menu leMenu("../MenuCPP/menu.txt");
-    do
-    {
-        choix = leMenu.Afficher();
-        switch (choix)
+    try {
+        Menu leMenu("../MenuCPP/menu.txt");
+        do
         {
-        case OPTION_1:
-            cout << "Vous avez choisi l'option n°1" << endl;
-            Menu::AttendreAppuiTouche();
-            break;
-        case OPTION_2:
-            cout << "Vous avez choisi l'option n°2" << endl;
-            Menu::AttendreAppuiTouche();
-            break;
-        case OPTION_3:
-            cout << "Vous avez choisi l'option n°3" << endl;
-            Menu::AttendreAppuiTouche();
-            break;
-        case OPTION_4:
-            cout << "Vous avez choisi l'option n°4" << endl;
-            Menu::AttendreAppuiTouche();
-            break;
-        case OPTION_5:
-            cout << "Vous avez choisi l'option n°5" << endl;
-            Menu::AttendreAppuiTouche();
-            break;
-        }
+            choix = leMenu.Afficher();
+            switch (choix)
+            {
+            case OPTION_1:
+                cout << "Vous avez choisi l'option n°1" << endl;
+                Menu::AttendreAppuiTouche();
+                break;
+            case OPTION_2:
+                cout << "Vous avez choisi l'option n°2" << endl;
+                Menu::AttendreAppuiTouche();
+                break;
+            case OPTION_3:
+                cout << "Vous avez choisi l'option n°3" << endl;
+                Menu::AttendreAppuiTouche();
+                break;
+            case OPTION_4:
+                cout << "Vous avez choisi l'option n°4" << endl;
+                Menu::AttendreAppuiTouche();
+                break;
+            case OPTION_5:
+                cout << "Vous avez choisi l'option n°5" << endl;
+                Menu::AttendreAppuiTouche();
+                break;
+            }
 
-    } while(choix != QUITTER);
+        } while(choix != QUITTER);
+    } catch (std::runtime_error e) {
+        cout << e.what() << endl;
+        exit(EXIT_FAILURE);
+    }
+
     return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-int main()
-{
-    int choix;
-    Menu leMenu("text.txt");
-    do
-    {
-        choix = leMenu.Afficher();
-        switch (choix)
-        {
-        case OPTION_1:
-                    cout << "Vous avez choisi l'option n°1" << endl;
-                    Menu::AttendreAppuiTouche();
-                    break;
-                case OPTION_2:
-                    cout << "Vous avez choisi l'option n°2" << endl;
-                    Menu::AttendreAppuiTouche();
-                    break;
-                case OPTION_3:
-                    cout << "Vous avez choisi l'option n°3" << endl;
-                    Menu::AttendreAppuiTouche();
-                    break;
-                case OPTION_4:
-                    cout << "Vous avez choisi l'option n°4" << endl;
-                    Menu::AttendreAppuiTouche();
-                    break;
-                }
-    } while(choix != QUITTER);
-    return 0;
-}
-*/
+//int main()
+//{
+//    int choix;
+//    Menu leMenu("text.txt");
+//    do
+//    {
+//        choix = leMenu.Afficher();
+//        switch (choix)
+//        {
+//        case OPTION_1:
+//                    cout << "Vous avez choisi l'option n°1" << endl;
+//                    Menu::AttendreAppuiTouche();
+//                    break;
+//                case OPTION_2:
+//                    cout << "Vous avez choisi l'option n°2" << endl;
+//                    Menu::AttendreAppuiTouche();
+//                    break;
+//                case OPTION_3:
+//                    cout << "Vous avez choisi l'option n°3" << endl;
+//                    Menu::AttendreAppuiTouche();
+//                    break;
+//                case OPTION_4:
+//                    cout << "Vous avez choisi l'option n°4" << endl;
+//                    Menu::AttendreAppuiTouche();
+//                    break;
+//                }
+//    } while(choix != QUITTER);
+//    return 0;
+//}
